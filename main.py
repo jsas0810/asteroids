4b023607235326be287d2fa59e484b06131cc75a
jsas0810 <jsas0810@gmail.com>
2025-02-06 13:20:28 -0500

    B: done Ch2. L4 Sprites

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
@@ -15,5 +17,6 @@ def main():
        				return
 		screen.fill((0, 0, 0))
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000 
 if __name__ == "__main__":
     main()
