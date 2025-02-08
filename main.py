78251b8e20beb481d839c531dddc073009ce277c
jsas0810 <jsas0810@gmail.com>
2025-02-08 10:11:43 -0500

    E: got asteroids to spawn

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import *
+from asteroidfield import *
+
+
 def main():
-	pygame.init
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
-	clock = pygame.time.Clock()
-	dt = 0
-	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	while True:
-		for event in pygame.event.get():
-  			if event.type == pygame.QUIT:
-       				return
-		screen.fill((0, 0, 0))
-		player.update(dt)
-		player.draw(screen)
-		pygame.display.flip()
-		dt = clock.tick(60) / 1000 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
